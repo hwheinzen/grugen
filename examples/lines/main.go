e176877afb76f841629f9d4d2ed15cec13fbdf9a
hwheinzen <hwheinzen@bitloeffel.de>
2019-11-26 16:54:07 +0100

    Add files via upload

---
+++ examples/lines/main.go
@@ -0,0 +1,72 @@
+// Example: lines
+
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+const pgmname = "lines"
+
+func main() {
+	file := args()
+	fin, err := os.Open(file)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer fin.Close()
+	in := bufio.NewReader(fin)
+
+	fout, err := os.Create("list.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer fout.Close()
+	out := bufio.NewWriter(fout)
+
+	err = conbreak(in, out)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return
+}
+
+// Args liest das erste String-Arguments und prüft, ob es ein gültiger
+// Dateiname ist.
+func args() (filename string) {
+	var help bool
+	flag.BoolVar(&help, "help", false, "usage information")
+	flag.Usage = usage
+	flag.Parse()
+
+	if help {
+		usage()
+		os.Exit(0)
+	}
+
+	if len(flag.Args()) < 1 {
+		log.Fatalln("1 argument wanted -", len(flag.Args()), "arguments found")
+	}
+
+	filename = flag.Args()[0]
+	fi, err := os.Stat(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if fi.IsDir() {
+		log.Fatalln(fi.Name(), "is not a file")
+	}
+	if fi.Size() == 0 {
+		log.Fatalln(fi.Name(), "is empty")
+	}
+	return
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "usage:", pgmname, "-help | input_filename")
+	flag.PrintDefaults()
+}
